6d57eeb530a6df4bfd4588cd80dda12fca87f925
Karthik B <karthikb351@gmail.com>
2021-08-09 22:46:00 +0530

    wip

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const getRankDataFromTrackerGG = (
-    platform: string,
-    id: string
-  ) => {
-    return axios.get(
+export const getRankDataFromTrackerGG = (platform: string, id: string) => {
+  return axios
+    .get(
       `https://api.tracker.gg/api/v2/rocket-league/standard/profile/${platform}/${id}`
-    );
-  };
+    )
+    .then(response => response.data);
+};
