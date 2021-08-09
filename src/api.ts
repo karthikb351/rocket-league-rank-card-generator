d4fbad3656557717aaf2d64751ff5e7259049e2f
Karthik Balakrishnan <karthikb351@gmail.com>
2021-08-09 16:15:00 +0000

    wip

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const getRankDataFromTrackerGG = (
+    platform: string,
+    id: string
+  ) => {
+    return axios.get(
+      `https://api.tracker.gg/api/v2/rocket-league/standard/profile/${platform}/${id}`
+    );
+  };
